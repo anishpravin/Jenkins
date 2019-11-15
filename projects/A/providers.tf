96e5c0cf092e810025233bab2a36ca1a54911868
anishpravin <anishpravin1993@gmail.com>
2019-11-15 00:21:13 +0000

    def

--- projects/A/providers.tf
+++ projects/A/providers.tf
@@ -1,5 +1,3 @@
 provider "aws" {
-  aws_access_key_id = AKIAJC6R5NJKEAGP2V3A
-  aws_secret_access_key = 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
   region = "us-east-2"
 }
--- projects/B/providers.tf
+++ projects/B/providers.tf
@@ -1,6 +1,4 @@
-provider "aws" {
-  aws_access_key_id = AKIA5KJYWJJ3SEUMBFRD
-  aws_secret_access_key = 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
+provider "aws" { 
   region = "us-east-2"
 }
 
