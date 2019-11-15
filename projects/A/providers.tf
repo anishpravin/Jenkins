f566824348e07c847273bcc8aab705356e8c36e5
anishpravin <anishpravin1993@gmail.com>
2019-11-15 00:31:54 +0000

    dcx

--- projects/A/providers.tf
+++ projects/A/providers.tf
@@ -1,3 +1,5 @@
 provider "aws" {
-  region = "us-east-2"
+  region     = "us-east-2"
+  access_key = "AKIAJC6R5NJKEAGP2V3A"
+  secret_key = "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"
 }
--- projects/B/providers.tf
+++ projects/B/providers.tf
@@ -1,4 +1,5 @@
-provider "aws" { 
-  region = "us-east-2"
+provider "aws" {
+  region     = "us-east-2"
+  access_key = "AKIAJC6R5NJKEAGP2V3A"
+  secret_key = "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"
 }
-
