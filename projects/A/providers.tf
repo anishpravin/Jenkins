647e41f0b3de405fb12b9b22cd1803d4b4c5a338
anishpravin <anishpravin1993@gmail.com>
2019-11-15 00:13:17 +0000

    df

--- projects/A/providers.tf
+++ projects/A/providers.tf
@@ -1,7 +1,5 @@
-
-
 provider "aws" {
-  aws_access_key_id = AKIA5KJYWJJ3SEUMBFRD
-  aws_secret_access_key = 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
+  aws_access_key_id = AKIAJC6R5NJKEAGP2V3A
+  aws_secret_access_key = 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
   region = "us-east-2"
 }
